b7323338d225be0d3860bd57e37fcb268d9bce9a
Jonas Friedann <j@frd.mn>
2014-06-28 15:22:12 +0200

    Fix function to determine hostname

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ $(function() {
 	var currentMarker;
 	
 	var assetsUrl = function() {
-		return window.location.host == 'localhost' ? '' : 'https://s3-eu-west-1.amazonaws.com/gtavmap/';
+		return window.location.hostname == 'localhost' ? '' : 'https://s3-eu-west-1.amazonaws.com/gtavmap/';
 	};
 
 	Handlebars.registerHelper('assetsUrl', assetsUrl);
